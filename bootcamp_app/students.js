0d0c33e892dd83f81247989261dd69557db7d3e5
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-09-21 15:44:40 -0400

    added two new tables to database and data to the tables

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,18 +8,17 @@ const pool = new Pool({
 });
 
 pool
-  .query(
-    `
+  .query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students 
 JOIN cohorts ON cohorts.id = cohort_id
-LIMIT 5;
-`
-  )
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
-        `${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
       );
     });
-  });
+  }) .catch(err => console.error('query error', err.stack));
