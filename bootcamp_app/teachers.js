76cddb1d7032370c72fcca35d4fe5628dc4d5d14
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-09-21 20:56:14 -0400

    added query to collect teachers with assistance request during a cohort from the database

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,7 +9,16 @@ const pool = new Pool({
 
 pool
   .query(`  
-  
-  
-  
+  SELECT DISTINCT cohorts.name as cohort, teachers.name as teacher
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
+  ORDER BY teacher;
   `)
+  .then((res) => {
+    res.rows.forEach((row) => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  });
